7eafac24de134745e97a104c45b9723714076ad0
SamakFox <32133264+LittleSamakFox@users.noreply.github.com>
2018-01-05 03:42:07 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 ﻿<?PHP
 @session_start();
 ?>
-<?PHP
-// Fix for removed Session functions 
-	function fix_session_register(){ 
-		function session_register(){ 
-			$args = func_get_args(); 
-			foreach ($args as $key){ 
-				$_SESSION[$key]=$GLOBALS[$key]; 
-			} 
-		} 
-		function session_is_registered($key){ 
-			return isset($_SESSION[$key]); 
-		} 
-		function session_unregister($key){ 
-			unset($_SESSION[$key]); 
-		} 
-	} 
-	if (!function_exists('session_register')) fix_session_register(); 
-?>
 <html lang="ko">
 	<head>
 		<meta charset = "utf-8">
